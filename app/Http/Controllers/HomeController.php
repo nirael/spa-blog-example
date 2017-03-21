6a7c4a013bf88dddc87597e1777af8816f61b10a
phoenix0101 <prestonnull0@gmail.com>
2017-03-21 19:09:48 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//use EllipseSynergie\ApiResponse\Contracts\Response;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Request as R;
@@ -11,22 +10,7 @@ use \App\Events\BanListEvent;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
    
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-    public function index()
-    {
-        return view('home');
-    }
-     */
     public function __construct(){
          event(new BanListEvent);
     }
