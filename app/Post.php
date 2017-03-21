a91bb58445317c10d5dcf99c8df935193b826d62
phoenix0101 <prestonnull0@gmail.com>
2017-03-21 18:54:44 +0200

    Update Post.php

--- app/Post.php
+++ app/Post.php
@@ -11,9 +11,6 @@ class Post extends Model
     public function category(){
     	return $this->belongsTo('App\Category','cat_id');
     }
-   /* public function cat_name(){
-    	return Category::find($this->cat_id)->name;
-    }*/
     public function comments(){
     	return $this->hasMany('App\Comment','post_id','id');
     }
@@ -21,13 +18,5 @@ class Post extends Model
     	return static::all(['id','name'])->take(-5);
     	
     }
-    public static function dates($three=false){
-    	$val = static::all(['created_at'])->unique()->map(function($obj){
-    		return ['pres' => $obj->created_at->format("F, Y"),'link' =>$obj->created_at->format("FY")];
-    	});
-    	return $three?$val->take(-3):$val;
-    }
-    public function excerpt($s=1000){
-        return substr($this->content, 0,$s) . "...";
-    }
+ 
 }
